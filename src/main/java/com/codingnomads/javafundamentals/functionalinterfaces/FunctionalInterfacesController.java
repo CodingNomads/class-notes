195ef4b3a4c7874aec65af7fe9fc251719a594a0
Roi Otero Morales <roioteromorales@gmail.com>
2018-10-04 21:53:18 -0500

    Functional Interfaces Fix

--- src/main/java/com/codingnomads/javafundamentals/functionalinterfaces/FunctionalInterfacesController.java
+++ src/main/java/com/codingnomads/javafundamentals/functionalinterfaces/FunctionalInterfacesController.java
@@ -8,18 +8,55 @@ import java.util.function.Supplier;
 public class FunctionalInterfacesController {
 
     public static void main(String[] args) {
+        useConsumer();
+        useSupplier();
+        useFunction();
+        usePredicate();
+    }
 
-        Consumer<String> consumer = s -> System.out.println(s);
-        consumer.accept("Hello");
-
-        Supplier<String> supplier = () -> "new String!";
-        String supplierResult = supplier.get();
-
-        Function<String, Integer> function = string -> new Integer(string);
-        Integer functionResult = function.apply("2");
-
+    private static void usePredicate() {
+        Predicate<String> predicateAsAnonymousClass = new Predicate<String>() {
+            @Override
+            public boolean test(String s) {
+                return s.equals("Java");
+            }
+        };
         Predicate<String> predicate = s -> s.equals("Java");
         boolean testResult = predicate.test("C#");
+        System.out.println(testResult);
+    }
 
+    private static void useFunction() {
+        Function<String, Integer> functionAsAnonymousClass = new Function<String, Integer>() {
+            @Override
+            public Integer apply(String s) {
+                return new Integer(s);
+            }
+        };
+        Function<String, Integer> function = string -> new Integer(string);
+        Integer functionResult = function.apply("2");
+        System.out.println("The function returned: " + functionResult);
+    }
+
+    private static void useSupplier() {
+        Supplier<String> supplierAsAnonymousClass = new Supplier<String>() {
+            @Override
+            public String get() {
+                return "new String!";
+            }
+        };
+        Supplier<String> supplier = () -> "new String!";
+        String supplierResult = supplier.get();
+    }
+
+    private static void useConsumer() {
+        Consumer<String> consumerAsAnonymousClass = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        };
+        Consumer<String> consumer = input -> System.out.println(input);
+        consumer.accept("Hello");
     }
 }
